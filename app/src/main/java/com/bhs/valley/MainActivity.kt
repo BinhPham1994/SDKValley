4ee9ff13108e834d3d2c807b4a144a34af265833
phamvanbinh1909 <phamvanbinh1909@gmail.com>
2023-03-23 13:22:42 +0700

    cmt

--- app/src/main/java/com/bhs/valley/MainActivity.kt
+++ app/src/main/java/com/bhs/valley/MainActivity.kt
@@ -3,9 +3,6 @@ package com.bhs.valley
 import android.content.Context
 import android.widget.Toast
 
-
-
-
 class MainActivity {
     fun showMessage(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
