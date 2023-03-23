8817e72a568dc840cb21d7b10bed2a1939859e1c
phamvanbinh1909 <phamvanbinh1909@gmail.com>
2023-03-23 15:56:30 +0700

    cmt

---
+++ app/src/main/java/com/bhs/valley/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bhs.valley;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sdkvalley.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.main_layout);
+    }
+}
